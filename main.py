82e19ba345197b100b835745a580400a1b156a36
alexanderdamiani <alexander.damiani15@gmail.com>
2021-02-07 23:07:41 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import math
+
+
+def add(a, b) -> int:
+    return math.floor(a + b)
+
+
+def to_sentence(s) -> str:
+    s = s.capitalize()
+
+    if s.endswith('.'):
+        return s
+    else:
+        return s + '.'
